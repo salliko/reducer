19f5390bfaaea7caf991e7f03b50927d52d5a7ae
grebennikov_iy <grebennikov_iy@magnit.ru>
2022-02-07 16:36:05 +0300

    Инкремент 2

---
+++ cmd/shortener/main_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestIndexHandle(t *testing.T) {
+	request := httptest.NewRequest(http.MethodPost, "/", strings.NewReader("http://ya.ru"))
+	w := httptest.NewRecorder()
+	h := http.HandlerFunc(IndexHandle)
+	h.ServeHTTP(w, request)
+	res := w.Result()
+
+	if res.StatusCode != 201 {
+		t.Errorf("Expected status code %d, got 201", res.StatusCode)
+	}
+}
