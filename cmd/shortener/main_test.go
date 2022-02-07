151c540f413c91344a436612e968ec8291e6a82d
grebennikov_iy <grebennikov_iy@magnit.ru>
2022-02-07 16:59:22 +0300

    fix: добавил закрытие тела запроса

--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -13,6 +13,7 @@ func TestIndexHandle(t *testing.T) {
 	h := http.HandlerFunc(IndexHandle)
 	h.ServeHTTP(w, request)
 	res := w.Result()
+	defer res.Body.Close()
 
 	if res.StatusCode != 201 {
 		t.Errorf("Expected status code %d, got 201", res.StatusCode)
